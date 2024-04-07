2fc8e5633908bb38e4094692ccd68564b46021c3
Steffen Vogel <post@steffenvogel.de>
2024-04-07 13:42:49 +0200

    ethercat: add kernel module

--- pkgs/by-name/et/ethercat/package.nix
+++ pkgs/by-name/et/ethercat/package.nix
@@ -6,7 +6,133 @@
   stdenv,
   fetchFromGitLab,
   gitUpdater,
+  kernel,
 }:
+let
+  deviceKernelCompatabilities = {
+    "6.6" = [ "igc" ];
+    "6.4" = [
+      "e100"
+      "igc"
+    ];
+    "6.1" = [
+      "8139too"
+      "bcmgenet"
+      "e100"
+      "e1000"
+      "e1000e"
+      "igb"
+      "igc"
+      "r8169"
+    ];
+    "5.15" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "igb"
+      "r8169"
+    ];
+    "5.14" = [
+      "8139too"
+      "bcmgenet"
+      "e100"
+      "e1000"
+      "e1000e"
+      "igb"
+      "igc"
+      "r8169"
+    ];
+    "5.10" = [
+      "8139too"
+      "bcmgenet"
+      "e100"
+      "e1000"
+      "e1000e"
+      "igb"
+      "r8169"
+    ];
+    "5.4" = [
+      "e100"
+      "e1000e"
+    ];
+    "4.19" = [ "igb" ];
+    "4.4" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "igb"
+      "r8169"
+    ];
+    "3.18" = [ "igb" ];
+    "3.16" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.14" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.12" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.10" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.8" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.6" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.4" = [
+      "8139too"
+      "e100"
+      "e1000"
+      "e1000e"
+      "r8169"
+    ];
+    "3.2" = [
+      "8139too"
+      "e1000e"
+      "r8169"
+    ];
+    "3.0" = [
+      "8139too"
+      "e100"
+      "e1000"
+    ];
+  };
+
+  kernelVersionMajorMinor = builtins.concatStringsSep "." (
+    builtins.take 2 (builtins.splitVersion kernel.version)
+  );
+
+  supportedDrivers = deviceKernelCompatabilities.${kernelVersionMajorMinor};
+in
 stdenv.mkDerivation (finalAttrs: {
   pname = "ethercat";
   version = "1.6-alpha";
@@ -19,15 +145,54 @@ stdenv.mkDerivation (finalAttrs: {
   };
 
   separateDebugInfo = true;
+  hardeningDisable = [
+    "pic"
+    "format"
+  ];
 
   nativeBuildInputs = [
     autoreconfHook
     pkg-config
+    kernel.moduleBuildDependencies
+  ];
+
+  outputs = [
+    "bin"
+    "dev"
+    "kmod"
   ];
 
   configureFlags = [
+    # Components
+    "--enable-tool=yes"
     "--enable-userlib=yes"
-    "--enable-kernel=no"
+    "--enable-kernel=yes"
+
+    # Features
+    "--enable-eoe=yes=yes"
+    "--enable-cycles=yes=yes"
+    "--enable-rtmutex=yes"
+    "--enable-hrtimer=yes"
+    "--enable-regalias=yes"
+    "--enable-refclkop=yes"
+
+    # Kernel
+    "--with-linux-dir=${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "--module-dir=$$kmod"
+
+    # Debugging
+    "--enable-debug-if=yes"
+    "--enable-debug-ring=yes"
+  ] ++ builtins.map (driver: "--enable-${driver}=yes") supportedDrivers;
+
+  buildFlags = [
+    "all"
+    "modules"
+  ];
+
+  installTargets = [
+    "modules_install"
+    "install"
   ];
 
   passthru.updateScript = gitUpdater { };
@@ -38,6 +203,10 @@ stdenv.mkDerivation (finalAttrs: {
     changelog = "https://gitlab.com/etherlab.org/ethercat/-/blob/${finalAttrs.version}/NEWS";
     license = licenses.gpl2Plus;
     maintainers = with maintainers; [ stv0g ];
-    platforms = [ "x86_64-linux" ];
+    platforms = platforms.linux;
+    outputsToInstall = [
+      "bin"
+      "out"
+    ];
   };
 })
